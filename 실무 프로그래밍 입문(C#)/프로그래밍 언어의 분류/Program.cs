0e0dd4c9f268eee156b711d4f51669e633b8c27f
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-14 23:11:13 +0900

    Hello world

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 프로그래밍_언어의_분류
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
