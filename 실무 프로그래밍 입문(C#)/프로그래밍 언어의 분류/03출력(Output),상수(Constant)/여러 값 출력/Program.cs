d4a22317928e1cbdcae3e7c5ebcb6732d834d8e8
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-16 00:08:07 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/03출력(Output),상수(Constant)/여러 값 출력/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 여러_값을_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(30);
+
+            Console.WriteLine(30.1);
+
+            Console.WriteLine(30.2f);
+
+            Console.Write(true);
+
+            Console.Write("p");
+        }
+    }
+}
