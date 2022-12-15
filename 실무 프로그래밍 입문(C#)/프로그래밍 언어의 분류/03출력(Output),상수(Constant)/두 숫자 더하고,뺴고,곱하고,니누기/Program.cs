4c278287182ded4426a17d6ebfe88b86395219cc
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-16 00:07:27 +0900

    사칙연산 소스코드

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/03출력(Output),상수(Constant)/두 숫자 더하고,뺴고,곱하고,니누기/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace 두_숫자_더하고_뺴고_곱하고_니누기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //두 숫자 더하기
+            Console.Write("10 + 20 =");
+            Console.WriteLine(10 + 20);
+
+            //두 숫자 뺴기
+            Console.Write("10 - 20 =");
+            Console.WriteLine(10 - 20);
+
+            // 두 숫자 곱하기
+            Console.Write("10 * 20 =");
+            Console.WriteLine(10 * 20);
+
+            // 두 숫자 나누기
+            Console.Write("10 / 20 =");
+            Console.WriteLine(10 / 20);
+        }
+    }
+}
