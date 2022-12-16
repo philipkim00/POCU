aba3d7c4effd4250e949f7d4dbb59da0aada4449
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-16 22:20:42 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/04변수(Variable),기본자료형(Primitive Types)/두 변수의 계산/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 두_변수의_계산
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 10.0f; //첫 번쨰 변수
+            float num2 = 20.0f; //두 반쨰 변수
+
+            //두 숫자 더하기
+            Console.Write(num1 + "+" + num2 + "=");
+            Console.WriteLine(num1 + num2);
+
+            //두 숫자 뺴기
+            Console.Write(num1 + "-" + num2 + "=");
+            Console.WriteLine(num1 - num2);
+
+            //두 숫자 곱하기
+            Console.Write(num1 + "*" + num2 + "=");
+            Console.WriteLine(num1 * num2);
+
+            //두 숫자 나누기
+            Console.Write(num1 + "/" + num2 + "=");
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
