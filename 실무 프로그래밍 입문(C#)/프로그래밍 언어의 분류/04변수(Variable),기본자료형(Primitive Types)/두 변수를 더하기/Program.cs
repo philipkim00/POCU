1c329a9e8e04298c666e2c99dce79865fc1cc716
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-16 22:20:28 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/04변수(Variable),기본자료형(Primitive Types)/두 변수를 더하기/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 두_변수를_더하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.Write(x + "+" + y + "=");
+            Console.Write(x + y);
+        }
+    }
+}
