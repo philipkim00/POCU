b63baf332c8e80e86ade32fb86ceaf48427f98ae
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-16 22:20:13 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/04변수(Variable),기본자료형(Primitive Types)/기본자료형/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 기본자료형
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 2; 
+            int int2 = 10;
+
+            float float1 = 10.23f;
+            float float2 = 3.4f;
+
+            double double1 = 11.0;
+            double double2 = 5.234;
+
+            char char1 = 'x';
+            char char2 = '$';
+
+            Console.WriteLine(int1 + "+" + int2 + "=" + (int1 + int2));
+            Console.WriteLine(int1 + "-" + int2 + "=" + (int1 - int2));
+            Console.WriteLine(float1 + "+" + float2 + "=" + (float1 + float2));
+            Console.WriteLine(double1 + "+" + double2 + "=" + (double1 + double2));
+            Console.WriteLine(double1 + "-" + double2 + "=" + (double1 - double2));
+        }
+    }
+}
