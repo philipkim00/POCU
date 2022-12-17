aefe81beb0af684fbde21cb74f3a145d8e64e4de
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-17 20:23:26 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/04변수(Variable),기본자료형(Primitive Types)/상수 10의 다양한 표현/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 상수_10의_다양한_표현
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intNum = 10;
+            int binaryNum = 0b10;
+            int hexNum = 0x10;
+            uint uIntNum = 10u;
+            float floatNum = 10.0f;
+            double doubleNum = 10.0;
+            string stringNum = "10";
+
+            Console.WriteLine(intNum);
+            Console.WriteLine(binaryNum);
+            Console.WriteLine(hexNum);
+            Console.WriteLine(uIntNum);
+            Console.WriteLine(floatNum);
+            Console.WriteLine(doubleNum);
+            Console.WriteLine(stringNum);
+
+        }
+    }
+}
