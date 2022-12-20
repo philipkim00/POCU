76e8765811516c1582e81837010fa48e1ed63102
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-20 18:04:16 +0900

    두 double형 덧셈

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/05형변환(Type Conversion),연산자(Operator)1/두 double 형 수를 더하기/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 두_double_형_수를_더하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 10.9;
+            double num2 = 52.16;
+            Console.WriteLine("일반 덧셈 결과: " + (num1 + num2));
+
+            int result = (int)num1 + (int)num2;
+            Console.WriteLine("명시적 형변환 덧셈 결과: " + result);
+        }
+    }
+}
