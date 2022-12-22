6e068e35b2158a9a49346eb1f013563a4dab1fd8
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-22 22:25:09 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/두 문자열이 같은지 확인하기/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 두_문자열이_같은지_확인하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message1 = "Hello";
+            string message2 = "Hello";
+
+            Console.WriteLine(message1 == message2);
+        }
+    }
+}
