c80df7d230e63fbff2e1c1bca162d1f9e861ae94
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-22 22:24:39 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/게임 승률 출력하기/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 게임_승률_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studnetName1 = "Kim Leon";
+            float winRate1 = 30.2351f;
+
+            string studentName2 = "Lulu";
+            float winRate2 = 70.11f;
+
+            Console.WriteLine("{0}    {1}\n", "Name", "Win Rate");
+            Console.WriteLine("{0}    {1}\n", studnetName1, winRate1);
+            Console.WriteLine("{0}    {1}\n", studentName2, winRate2);
+        }
+    }
+}
