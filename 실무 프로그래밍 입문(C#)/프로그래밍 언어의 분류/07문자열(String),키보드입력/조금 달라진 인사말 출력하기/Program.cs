ff9144976e837ffc3ba928092762961e0f25976b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-22 22:26:16 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/조금 달라진 인사말 출력하기/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace 조금_달라진_인사말_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hi, everybody!";
+            Console.WriteLine(greeting);
+        }
+    }
+}
