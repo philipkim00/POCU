48469d1ccb542d2e676b25f5921caa1bb60147e1
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-22 22:26:37 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/줄바꿈, 탭 출력하기/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace 줄바꿈__탭_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome\nPope\nBye~\n");
+
+            Console.WriteLine("Welcome\tPope");
+        }
+    }
+}
