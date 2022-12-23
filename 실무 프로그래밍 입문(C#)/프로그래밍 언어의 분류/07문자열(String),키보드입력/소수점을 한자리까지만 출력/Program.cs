2845064f864889f331512bb567246a04c4951e01
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:50:39 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/소수점을 한자리까지만 출력/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 소수점을_한자리까지만_출력
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", 3.14159265359);
+            Console.WriteLine("{0:f4}", 3.14159265359);
+            Console.WriteLine("{0:f3}", 3.14159265359);
+            Console.WriteLine("{0:f2}", 3.14159265359);
+            Console.WriteLine("{0:f1}", 3.14159265359);
+
+            Console.WriteLine("{0}", 10.1);
+            Console.WriteLine("{0:f2}", 10.1);
+            Console.WriteLine("{0:f3}", 10.1);
+            Console.WriteLine("{0:f4}", 10.1);
+
+            Console.WriteLine("{0}", 200);
+            Console.WriteLine("{0:f1}", 200);
+            Console.WriteLine("{0:f2}", 200);
+            Console.WriteLine("{0:f3}", 200);
+        }
+    }
+}
+
