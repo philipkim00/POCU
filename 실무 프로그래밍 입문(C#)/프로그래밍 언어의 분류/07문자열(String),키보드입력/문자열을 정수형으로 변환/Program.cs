70b9da6314dcb9432b9fe9bd57d35f871b6de7b1
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:51:53 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/문자열을 정수형으로 변환/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 문자열을_정수형으로_변환
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            string numStr1 = Console.ReadLine();
+            int num1 = int.Parse(numStr1);
+
+            Console.WriteLine("Enter the second number: ");
+            string numStr2 = Console.ReadLine();
+            int num2 = int.Parse(numStr2);
+
+            int result = num1 + num2;
+            Console.WriteLine($"Num1: {num1}, Num2: {num2} = {result}");
+        }
+    }
+}
