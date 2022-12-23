93689b8ea1301b7eb00f708e74195ac71c8b373b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:50:01 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/스페이스 바를 이용해서 정렬/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 스페이스_바를_이용해서_정렬
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studnetName1 = "Kim Leon";
+            float winRate1 = 30.2351f;
+
+            string studentName2 = "Lulu";
+            float winRate2 = 70.11f;
+
+            Console.WriteLine("{0}     {1}\n", "Name", "Win Rate");
+            Console.WriteLine("{0} {1}\n", studnetName1, winRate1);
+            Console.WriteLine("{0}     {1}\n", studentName2, winRate2);
+        }
+    }
+}
