04f5264b0e33f2a70e25035712edb61b1f3cc062
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:49:27 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/10진수를 16진수로 출력하기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10진수를_16진수로_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", 10);
+            Console.WriteLine("{0:x}", 10); // 소문자
+            Console.WriteLine("{0:X}", 10); // 대문자
+            Console.WriteLine("{0:x1}", 10);
+            Console.WriteLine("{0:x2}", 10);
+            Console.WriteLine("{0:x3}", 10);
+            Console.WriteLine("{0:x4}", 10);
+        }
+    }
+}
