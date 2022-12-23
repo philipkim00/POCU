8a08d5cb3da1780c7504e0507c6022d7bd452866
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:55:11 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/08조건문/조건외 나머지 처리하는 계산기/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace 조건외_나머지_처리하는_계산기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string numStr1 = Console.ReadLine();
+            int num1 = int.Parse(numStr1);
+
+            Console.Write("Enter the second number: ");
+            string numStr2 = Console.ReadLine();
+            int num2 = int.Parse(numStr2);
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            if (op == "+")
+            {
+                Console.Write($"{num1} + {num2} = {num1 + num2}");
+            }
+            else if (op == "-")
+            {
+                Console.Write($"{num1} - {num2} = {num1 - num2}");
+            }
+            else if (op == "*")
+            {
+                Console.Write($"{num1} * {num2} = {num1 * num2}");
+            }
+            else if (op == "/") // 정확하지 않는 나숫셈
+            {
+                Console.Write($"{num1} / {num2} = {num1 / num2}");
+            }
+            else
+            {
+                Console.WriteLine($"You entered a wrong operator: {op}");
+            }
+        }
+    }
+}
