27d1aaf5e474c618a6301e1b4ed04bed37cf7798
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:55:45 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/08조건문/코드의 문제점 찾기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 코드의_문제점_찾기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your math score: ");
+            string scoreString = Console.ReadLine();
+            int score = int.Parse(scoreString);
+
+            if (score >= 70)
+            {
+                Console.WriteLine("Pizza time!!");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Buy some books");
+            }
+            else if (score >= 90)
+            {
+                Console.WriteLine("Buy a game console!!!!!");
+            }
+        }
+    }
+}
