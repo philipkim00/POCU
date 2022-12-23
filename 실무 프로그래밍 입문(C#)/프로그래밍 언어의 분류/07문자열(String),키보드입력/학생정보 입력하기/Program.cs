930572ce5fd716a6378efec2d04379d7829c3a36
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:52:09 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/학생정보 입력하기/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace 학생정보_입력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's fill out student information: ");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Student Number: ");
+            string studentNumberString = Console.ReadLine();
+            int studentNumber = int.Parse(studentNumberString);
+
+            Console.Write("Grade: ");
+            string gradeString = Console.ReadLine();
+            int grade = int.Parse(gradeString);
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"{name}\n{studentNumber}\n{grade}%");
+        }
+    }
+}
