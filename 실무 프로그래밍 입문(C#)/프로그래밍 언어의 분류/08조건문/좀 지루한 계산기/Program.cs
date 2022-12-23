3996a2ca2be470ef8c008c8d633115857dcc2167
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:55:32 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/08조건문/좀 지루한 계산기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 좀_지루한_계산기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine()); 
+            
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        }
+    }
+}
