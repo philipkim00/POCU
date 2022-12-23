82fd7fd4c75c6184aeceac986bc87f1982662f3b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:51:34 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/키보드로부터 두 숫자를 읽어 오기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 키보드로부터_두_숫자를_읽어_오기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number: ");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine($"Num1: {num1}, Num2: {num2}");
+        }
+    }
+}
