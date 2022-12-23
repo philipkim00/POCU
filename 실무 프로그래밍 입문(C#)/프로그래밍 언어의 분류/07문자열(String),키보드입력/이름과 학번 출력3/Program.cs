ac913061908a20f7ab05cc8c483d6ebbb98408ff
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:51:00 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/이름과 학번 출력3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 이름과_학번_출력3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lulu";
+            int id = 201900001;
+
+            //방법1
+            string messige1 = string.Format($"Student Name: {name} / Student ID: {id}");
+            Console.WriteLine(messige1);
+
+            //방법2
+            Console.WriteLine($"Student Name: {name} / Student ID: {id}");
+        }
+    }
+}
