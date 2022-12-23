a905fc0079fcd1a69c57818bbeecb44a0193b99b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:54:59 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/08조건문/계산기에 곱셈 나눗셈 뺄셈 추가2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace 계산기에_곱셈_나눗셈_뺄셈_추가2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            string numStr1 = Console.ReadLine();
+            int num1 = int.Parse(numStr1);
+
+            Console.Write("Enter the second number: ");
+            string numStr2 = Console.ReadLine();
+            int num2 = int.Parse(numStr2);
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            if (op == "+")
+            {
+                Console.Write($"{num1} + {num2} = {num1 + num2}");
+            }
+            else if (op == "-")
+            {
+                Console.Write($"{num1} - {num2} = {num1 - num2}");
+            }
+            else if (op == "*")
+            {
+                Console.Write($"{num1} * {num2} = {num1 * num2}");
+            }
+            else if (op == "/") // 정확하지 않는 나숫셈
+            {
+                Console.Write($"{num1} / {num2} = {num1 / num2}");
+            }
+        }
+    }
+}
