5051f5e66996ad608c9c573b190afbf0fe3bb545
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-23 22:50:17 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/07문자열(String),키보드입력/스페이스 바를 이용하지 않은 정렬/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 스페이스_바를_이용하지_않은_정렬
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studnetName1 = "Kim Leon";
+            float winRate1 = 30.2351f;
+
+            string studentName2 = "Lulu";
+            float winRate2 = 70.11f;
+
+            Console.WriteLine("{0,10}{1,15}\n", "Name", "Win Rate");
+            Console.WriteLine("{0,10}{1,15}\n", studnetName1, winRate1);
+            Console.WriteLine("{0,10}{1,15}\n", studentName2, winRate2);
+        }
+    }
+}
