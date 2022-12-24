875a93507cbd1fb0191d5c8beddb5e2e40eb7a17
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-24 21:58:18 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/09 논리(Logical) 및 조건(Conditional) 연산자/논리 부족한 사람이 짠 코드/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace 논리_부족한_사람이_짠_코드
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your math score: ");
+            string scoreString = Console.ReadLine();
+            int score = int.Parse(scoreString);
+
+            if (score >= 90)
+            {
+                Console.WriteLine("Game console");
+            }
+            else if (score < 90 && score >= 80)
+            {
+                Console.WriteLine("Comic books");
+            }
+            else if (score < 80 && score >= 70)
+            {
+                Console.WriteLine("Pizza");
+            }
+            else
+            {
+                Console.WriteLine("Sorry! Try next time!");
+            }
+        }
+    }
+}
