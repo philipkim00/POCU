d582f04d35113d1926d81e3981359c71e1200bb8
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-24 21:59:03 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/09 논리(Logical) 및 조건(Conditional) 연산자/올바른 수학 성적 보상 프로그램/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 올바른_수학_성적_보상_프로그램
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your math score: ");
+            string scoreString = Console.ReadLine();
+            int score = int.Parse(scoreString);
+
+            if (score >= 90)
+            {
+                Console.WriteLine("Game console");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("Comic books");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("Pizza");
+            }
+            else
+            {
+                Console.WriteLine("Sorry!");
+            }
+        }
+    }
+}
