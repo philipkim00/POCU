037c981dbec2911106742ea4c08d3a50455198ea
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-24 21:58:48 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/09 논리(Logical) 및 조건(Conditional) 연산자/둘 중 큰수를 출력하기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 둘_중_큰수를_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second numbert: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int max;
+            if (num1 > num2)
+            {
+                max = num1;
+            }
+            else
+            {
+                max = num2;
+            }
+
+            Console.WriteLine($"Max number is {max}");
+        }
+    }
+}
