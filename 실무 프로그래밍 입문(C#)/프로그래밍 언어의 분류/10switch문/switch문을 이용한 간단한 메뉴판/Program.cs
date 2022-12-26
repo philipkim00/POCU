b2444018ae9a197555b1c7a73bc8ad696a96519a
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 17:50:49 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/10switch문/switch문을 이용한 간단한 메뉴판/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch문을_이용한_간단한_메뉴판
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a meun");
+            Console.WriteLine("1. Cheese burger");
+            Console.WriteLine("2. Double cheesse burger");
+            Console.WriteLine("3. Veggie burger");
+            Console.WriteLine("4. Cheese & mushroom burger");
+
+            int menu = int.Parse(Console.ReadLine());
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("You selected a cheese burger");
+                    Console.WriteLine("Price: $3");
+                    break;
+                case 2:
+                    Console.WriteLine("You selected a double cheese burger");
+                    Console.WriteLine("Price: $4.50");
+                    break;
+                case 3:
+                    Console.WriteLine("You selected a veggie burger");
+                    Console.WriteLine("Price: $4");
+                    break;
+                case 4:
+                    Console.WriteLine("You selected a mushroom burger");
+                    Console.WriteLine("Price: $5");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a correct number(1~4)");
+                    break;
+            }
+
+        }
+    }
+}
