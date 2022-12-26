ad38ecfd5a17b9c7f5b156ff2b6e12655186ada6
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 21:40:58 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/11배열(Array)/char 배열로 이름 출력하기/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace char_배열로_이름_출력하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myName = new char[4];
+
+            myName[0] = 'L';
+            myName[1] = 'u';
+            myName[2] = 'l';
+            myName[3] = 'u';
+
+            Console.WriteLine($"My name is {myName[0]}{myName[1]}{myName[2]}{myName[3]}");
+        }
+    }
+}
