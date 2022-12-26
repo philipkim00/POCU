518ae86380994160c9435323b8e6e3c18e4f346b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 17:51:20 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/10switch문/switch문이 어떻게 동작하나요/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace switch문이_어떻게_동작하나요
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 2;
+
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("You selected a cheese burger");
+                    break;
+                case 2:
+                    Console.WriteLine("You selected a double cheese burger");
+                    break;
+                case 3:
+                    Console.WriteLine("Please enter a correct number");
+                    break;
+            }
+
+        }
+    }
+}
