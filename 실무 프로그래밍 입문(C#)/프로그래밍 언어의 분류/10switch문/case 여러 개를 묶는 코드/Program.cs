50612676caca42204b940ed01a9b3c55ca9ec4d5
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 17:50:15 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/10switch문/case 여러 개를 묶는 코드/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace case_여러_개를_묶는_코드
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your grade?");
+            Console.WriteLine("1. Elementary school(1-6 hrade)");
+            Console.WriteLine("2. Middle school(1-3 hrade)");
+            Console.WriteLine("3. High school(1-3 hrade)");
+            Console.WriteLine("4. College, university or graduated school student");
+
+            int answer = int.Parse(Console.ReadLine());
+            switch (answer)
+            {
+                case 1:
+                    Console.WriteLine("Fare: $1");
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("Farres: $1.50");
+                    break;
+                case 4:
+                    Console.WriteLine("Fare: $2");
+                    break;
+                default:
+                    Console.WriteLine("Fare: $3");
+                    break;
+            }
+
+        }
+    }
+}
