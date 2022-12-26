11f957a6ba36b126c5e1e29dcf8e0e91ee78d811
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 16:38:00 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/09 논리(Logical) 및 조건(Conditional) 연산자/표현식 평가순서/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace 표현식_평가순서
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 15;
+            int num3 = 2;
+
+            int max1 = num1;
+            if (num2 > max1)
+            {
+                max1 = num2;
+            }
+
+            if (num3 > max1)
+            {
+                max1 = num3;
+            }
+
+            Console.WriteLine($"max1: {max1}");
+
+            int max2 = num1;
+            max1 = num2 > max2 ? num2 : max2;
+            max2 = num3 > max2 ? num3 : max2;
+
+            int result1 = num1 * num2 + num2 - num3 * num1; // 5, 15 , 2 , 75  + 15 - 10 = 80
+            Console.WriteLine($"result1: {result1}");
+
+            int result2 = num1 * num2 + (num2 + num2 - num3) * num1; // 75 + (28) x5 = 140 + 75 =225
+            Console.WriteLine($"result2: {result2}");
+
+            int result3 = num1 / (num2 + num1) + num1++; // 5 / 20 == 0 , 0 + 5++ result3 5 num1 == 6
+            Console.WriteLine($"result3: {result3}");
+
+            int result4 = num3 >= num1 ? num1 : num3 + num1 * num2; // 90 + 2 == 92
+            Console.WriteLine($"result4: {result4}");
+
+            int result5 = num3 >= num1 ? num1 : (num3 + num1 * num2); 
+            Console.WriteLine($"result5: {result5}");
+        }
+    }
+}
