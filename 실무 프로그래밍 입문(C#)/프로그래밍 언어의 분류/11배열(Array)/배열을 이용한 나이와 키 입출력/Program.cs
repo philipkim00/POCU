3f6798c70247c18176e1744a9fa072f7edc05979
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 21:41:32 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/11배열(Array)/배열을 이용한 나이와 키 입출력/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace 배열을_이용한_나이와_키_입출력
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = new int[3];
+            float[] heights = new float[3];
+
+            Console.Write("Enter the first user's age: ");
+            ages[0] = int.Parse(Console.ReadLine());
+            Console.Write("Enter the first user's height(cm)");
+            heights[0] = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second user's age: ");
+            ages[1] = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second user's height(cm)");
+            heights[1] = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third user's age: ");
+            ages[2] = int.Parse(Console.ReadLine());
+            Console.Write("Enter the third user's height(cm)");
+            heights[2] = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Print result--------");
+            Console.WriteLine($"The first User: Age({ages[0]}), Height({heights[0]}cm)");
+            Console.WriteLine($"The first User: Age({ages[1]}), Height({heights[1]}cm)");
+            Console.WriteLine($"The first User: Age({ages[2]}), Height({heights[2]}cm)");
+        }
+    }
+}
