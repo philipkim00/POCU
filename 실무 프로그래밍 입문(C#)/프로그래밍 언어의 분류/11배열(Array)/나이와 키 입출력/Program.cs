0b14bc893441690b7b6713ad913ae0d5f3bc0bae
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 21:41:17 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/11배열(Array)/나이와 키 입출력/Program.cs
@@ -0,0 +1,30 @@
+﻿ using System;
+
+namespace 나이와_키_입출력
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first user's age: ");
+            int age1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the first user's height(cm)");
+            float height1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second user's age: ");
+            int age2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second user's height(cm)");
+            float height2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third user's age: ");
+            int age3 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the third user's height(cm)");
+            float height3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Print result-------------");
+            Console.WriteLine($"The first User: Age({age1}), Height({height1}cm)");
+            Console.WriteLine($"The first User: Age({age2}), Height({height2}cm)");
+            Console.WriteLine($"The first User: Age({age3}), Height({height3}cm)");
+        }
+    }
+}
