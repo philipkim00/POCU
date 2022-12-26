638dbb8e9f49ba2da61e55d0b97142264e4364c0
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 17:51:03 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/10switch문/switch문을 이용한 계산기/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace switch문을_잉요한_계산기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("num1: ");
+            string num1String = Console.ReadLine();
+            int num1 = int.Parse(num1String);
+
+            Console.WriteLine("num2: ");
+            string num2String = Console.ReadLine();
+            int num2 = int.Parse(num2String);
+
+            Console.WriteLine("operation (+,-,*,/): ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    break;
+                default:
+                    Console.WriteLine($"You entered a wwrong operator: {operation}");
+                    break;
+            }
+
+        }
+    }
+}
