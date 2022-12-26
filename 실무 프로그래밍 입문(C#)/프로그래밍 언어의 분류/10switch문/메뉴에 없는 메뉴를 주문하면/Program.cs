9d84598a968d7a664b90776b2da7a7db6b50364d
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-26 17:51:36 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/10switch문/메뉴에 없는 메뉴를 주문하면/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 메뉴에_없는_메뉴를_주문하면
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 3;
+
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("You selected a cheese burger");
+                    break;
+                case 2:
+                    Console.WriteLine("You selected a double cheese burger");
+                    break;
+                default:
+                    Console.WriteLine("Please enter a correct number");
+                    break;
+            }
+        }
+    }
+}
