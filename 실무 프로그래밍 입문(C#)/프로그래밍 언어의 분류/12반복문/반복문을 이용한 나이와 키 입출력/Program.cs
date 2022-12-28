1e5adfc5496f305c68d7da5f3c95940ee5c125f0
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:14:27 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/반복문을 이용한 나이와 키 입출력/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace 반복문을_이용한_나이와_키_입출력
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User count: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] ages = new int[length];
+            float[] heights = new float[length];
+
+            int totalAges = 0;
+            float totalHeights = 0.0f;
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter the first user's age: ");
+                ages[i] = int.Parse(Console.ReadLine());
+                totalAges += ages[i];
+
+                Console.Write("Enter the first user's height(cm)");
+                heights[i] = float.Parse(Console.ReadLine());
+                totalHeights += heights[i];
+            }
+
+            Console.WriteLine("Print result-------------");
+            for (int i = 0; i < length; ++i)
+            {
+                Console.WriteLine($"User{i}: Age{ages[i]}, Height{heights[i]}cm");
+            }
+
+            Console.WriteLine($"Total ages: {totalAges}");
+            Console.WriteLine($"Total heights: {totalHeights}");
+        }
+    }
+}
