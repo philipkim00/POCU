a6a96b041e825a8fbdaa4ae8d1d0c22681fa2199
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:11:44 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/while를 이용한 나이와 키의 종합을 구하는 프로그램/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace while를_이용한_나이와_키의_종합을_구하는_프로그램
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User count: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] ages = new int[length];
+            float[] heights = new float[length];
+
+            int totalAges = 0;
+            float totalHeights = 0.0f;
+            int count = 0;
+            while (count < length)
+            {
+                Console.Write("Enter the first user's age: ");
+                ages[count] = int.Parse(Console.ReadLine());
+                totalAges += ages[count];
+
+                Console.Write("Enter the first user's height(cm)");
+                heights[count] = float.Parse(Console.ReadLine());
+                totalHeights += heights[count];
+
+                ++count;
+            }
+            Console.WriteLine($"Total ages: {totalAges}");
+            Console.WriteLine($"Total heights: {totalHeights}");
+        }
+    }
+}
+
