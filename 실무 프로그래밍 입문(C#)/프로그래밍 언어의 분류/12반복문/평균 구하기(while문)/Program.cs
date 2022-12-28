ee132680bfb1ea8f6c5a733e66f98fe44d8104b5
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:15:06 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/평균 구하기(while문)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 평균_구하기_while문_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numer of scores: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] scores = new int[length];
+
+            int totalScores = 0;
+            int count = 0;
+            while (count < length)
+            {
+
+                Console.Write($"Enter your score({count + 1}): ");
+                scores[count] = int.Parse(Console.ReadLine());
+                totalScores += scores[count];
+                ++count;
+            }
+
+            Console.WriteLine($"Average = {totalScores / (float)length}");
+        }
+    }
+}
