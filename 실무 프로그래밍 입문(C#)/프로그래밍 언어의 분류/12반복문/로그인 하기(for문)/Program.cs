005b3bd3651a3b0ecc64107a6a7aa966dd667691
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:13:44 +0900

    Add files via upload

--- 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/로그인 하기(for문)/Program.cs
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/로그인 하기(for문)/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace 로그인_하기_for문_
 {
@@ -10,6 +6,23 @@ namespace 로그인_하기_for문_
     {
         static void Main(string[] args)
         {
+            string passcode = "0046a";
+
+            string userInput = "";
+
+            for (; ; )
+            {
+                Console.Write("Please enter the password: ");
+                userInput = Console.ReadLine();
+
+                if (passcode == userInput)
+                {
+                    Console.WriteLine("Correct! Welcome home!");
+                    break;
+                }
+            }
+            
+            Console.WriteLine("Wrong password!");
         }
     }
 }
