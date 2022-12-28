bbfd4c4dca814242ee17975f9ddb18672e4dc7c0
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:14:42 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/털리기 쉬운 로그인 프로그램/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 털리기_쉬운_로그인_프로그램
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string passcode = "0046a"; // 실제로 비밀번호는 이렇게 저장안함.
+            string userInput = "";
+
+            while (true)
+            {
+                Console.WriteLine("Please enter the password: ");
+                userInput = Console.ReadLine(); 
+
+                if (userInput == passcode) 
+                {
+                    Console.WriteLine("Correct! Welcome home!");
+                    break;
+                }
+
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
