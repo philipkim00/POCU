ce85e45e8a7016f881037fa999e98904066c0d12
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:11:26 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/do-while반복문을 이용한 나이와 키 프로그램/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace do_while반복문을_이용한_나이와_키_프로그램
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("User count: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] ages = new int[length];
+            float[] heights = new float[length];
+
+            int totalAges = 0;
+            float totalHeights = 0.0f;
+            int count = 0;
+            do
+            {
+                Console.Write("Enter the first user's age: ");
+                ages[count] = int.Parse(Console.ReadLine());
+                totalAges += ages[count];
+
+                Console.Write("Enter the first user's height(cm)");
+                heights[count] = float.Parse(Console.ReadLine());
+                totalHeights += heights[count];
+
+                ++count;
+            } while (count < length);
+
+
+            Console.WriteLine($"Total ages: {totalAges}");
+            Console.WriteLine($"Total heights: {totalHeights}");
+        }
+    }
+}
