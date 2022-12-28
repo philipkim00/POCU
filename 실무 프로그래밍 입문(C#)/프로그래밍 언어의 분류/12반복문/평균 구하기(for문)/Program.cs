77226b96f0521d3b1381f16350c6a219ac2248f5
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:14:54 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/평균 구하기(for문)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace 평균_구하기_for문_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numer of scores: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] scores = new int[length];
+
+            int totalScores = 0;
+            for (int i = 0; i < length; ++i)
+            {
+                Console.Write($"Enter your score({i + 1}): ");
+                scores[i] = int.Parse(Console.ReadLine());
+                totalScores += scores[i];
+            }
+
+            Console.WriteLine($"Average = {totalScores / (float)length}");
+        }
+    }
+}
