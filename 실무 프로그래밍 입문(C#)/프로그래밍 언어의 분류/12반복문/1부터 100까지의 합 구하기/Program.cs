8361a090e987a34fe659da1350e59ee968414706
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:11:08 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/1부터 100까지의 합 구하기/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1부터_100까지의_합_구하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX = 100;
+
+            Console.WriteLine($"Sum of 1 - {MAX}");
+
+            int sum = 0;
+            for (int i = 0; i < MAX; i++) //for (int i =1; i <= MAX; i++) 해도 상관은 없
+            {
+                sum += (i + 1); // sum += i 
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
