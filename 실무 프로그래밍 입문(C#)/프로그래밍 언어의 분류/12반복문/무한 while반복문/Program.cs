b7985e69aa0b28c89b1b0a76dc5f494a5aeb45a1
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-28 22:12:24 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/12반복문/무한 while반복문/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+
+namespace 무한_while반복문
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input Number: ");
+                string inputString = Console.ReadLine();
+                int input = int.Parse(inputString);
+
+                if (input == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Number in hex: 0x{0:x}", input);
+            }
+        }
+    }
+}
