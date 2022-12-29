38f5930ead037acee39bec2b5830c555f29d1239
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-29 21:08:43 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/13다차원 배열(Multidimensional Array)/구구단 프로그램/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 구구단_프로그램
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = new int[9, 9];
+
+           for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    table[i, j] = (i + 1) * (j + 1);
+                    Console.Write($"{(i+1),2} * {(j+1),-2} = {table[i,j],-3}");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
