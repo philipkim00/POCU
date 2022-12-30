32062115cb24acd6be483bc6e2a885465c6cb91e
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2022-12-30 22:30:31 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace 성적_갱신_및_평균_구하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] scores = { 100.0, 35.4, 75.4, 54.6, 99.5 };
+
+            double average = 0.0;
+            for (int i = 0; i < scores.Length; i++) 
+            {
+                average += scores[i];
+            }
+            average /= scores.Length;
+            average = (int)(average * 10) / 10.0;
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"{scores.Length} scores average: {average:f1}");
+            Console.WriteLine("---------------------------");
+
+            while (true)
+            {
+                for (int i = 0; i< scores.Length; ++i) 
+                {
+                    Console.WriteLine($"Score[i]:  {scores[i]}");
+                }
+
+                Console.WriteLine($"While score do you want to update?(0~{scores.Length - 1})");
+                    
+                int index = int.Parse( Console.ReadLine() );
+
+                if (index < 0 || index >= scores.Length)
+                {
+                    Console.WriteLine("Wrong index.");
+                }
+                else
+                {
+                    Console.Write("Enter the score: ");
+                    scores[index] = double.Parse(Console.ReadLine());
+                    average = 0.0;
+                    for (int i = 0; i < scores.Length; ++i)
+                    {
+                        average += scores[i];
+                    }
+                    average /= scores.Length;
+                    average = (int)(average * 10) / 10.0;
+
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine($"{scores.Length} scores average: {average:f1}");
+                    Console.WriteLine("---------------------------");
+                }
+            }
+        }
+    }
+}
