829fe6a79e677d20daec8376e0968f694d24a626
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-02 21:07:07 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/함수를 사용하도록 수정한 평균 구하기/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace 함수를_사용하도록_수정한_평균_구하기
+{
+    internal class Program
+    {
+        static double Sum(double[] scores)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < scores.Length; ++i)
+            {
+                sum += scores[i];
+            }
+
+            return sum;
+        }
+
+        static double Average(double data, int count)
+        {
+            data /= count;
+            data = (int)(data * 10 + 0.5) / 10.0;
+            return data;
+        }
+
+        static void PrintAverage(double average, int count)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine($"{count} scroes average: {average:f1}");
+            Console.WriteLine("-------------------------");
+        }
+
+        static void Main(string[] args)
+        {
+            double[] scores = { 100.0, 35.4, 75.4, 54.6, 99.5 };
+            double average = Average(Sum(scores), scores.Length);
+            PrintAverage(average, scores.Length);
+
+            while (true)
+            {
+                for (int i = 0; i < scores.Length; ++i)
+                {
+                    Console.WriteLine($"Score{i}: {scores[i]}");
+                }
+
+                Console.WriteLine($"While score do you want to updata?(0~{scores.Length - 1})");
+                int index = int.Parse(Console.ReadLine());
+                if (index < 0 || index >= scores.Length)
+                {
+                    Console.WriteLine("Wrong index.");
+                }
+                else
+                {
+                    Console.Write("Entert the scores: ");
+                    scores[index] = double.Parse(Console.ReadLine());
+                    average = Average(Sum(scores), scores.Length);
+                    PrintAverage(average, scores.Length);
+                }
+            }
+        }
+
+    }
+}
