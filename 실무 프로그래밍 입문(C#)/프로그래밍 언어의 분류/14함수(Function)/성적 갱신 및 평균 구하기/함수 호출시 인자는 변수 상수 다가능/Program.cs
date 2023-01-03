8534d8a52a6d8597b54de3b17be4b5fcd5497b5d
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-03 20:23:36 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/함수 호출시 인자는 변수 상수 다가능/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 함수_호출시_인자는_변수_상수_다가능
+{
+    internal class Program
+    {
+        static int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+
+        static void Main(string[] args)
+        {
+            int result1 = Add(123, 589); // 상수 가능
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int result2 = Add(num1, num2); // 변수 가능
+        }
+    }
+}
