98b0a2da5752210dae86ddc52594b7d148861d51
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-03 20:23:23 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/함수 호출(void 반환형)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 함수_호출_void_반환형_
+{
+    internal class Program
+    {
+        static void PrintHello()
+        {
+            Console.WriteLine("Just hello!");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintHello();
+        }
+    }
+}
