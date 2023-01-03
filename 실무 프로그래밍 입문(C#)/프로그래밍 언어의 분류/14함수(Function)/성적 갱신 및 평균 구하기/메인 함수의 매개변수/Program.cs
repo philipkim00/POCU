eb6f576419997acc0c3ee408e1c49a14dd98f067
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-03 20:22:13 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/메인 함수의 매개변수/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 메인_함수의_매개변수
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for  (int i = 0; i< args.Length; ++i)
+            {
+                Console.WriteLine($"arhs[{i}] = {args[i]}");
+            }
+        }
+    }
+}
