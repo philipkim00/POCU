163ec9fadb397b85971dece74fa8a19dace8bd8f
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-03 20:22:26 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/선조건을 명시한 주석 예시/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 선조건을_명시한_주석_예시
+{
+    internal class Program
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="neumerator"></param>
+        /// <param name="denominator">no zero</param>
+        /// <returns></returns>
+        static float Divide(float numerator, float denominator)
+        {
+            return numerator / denominator;
+        }
+    }
+}
