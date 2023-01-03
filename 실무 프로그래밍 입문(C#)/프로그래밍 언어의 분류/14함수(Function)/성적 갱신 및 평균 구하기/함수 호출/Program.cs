6e683bebf3f0c4547b0210edf880bbf5dd443c1b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-03 20:23:11 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/함수 호출/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 함수_호출
+{
+    internal class Program
+    {
+        static int Add(int op1, int op2)
+        {
+            return op1 + op2;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Add(123, 589);
+        }
+    }
+}
