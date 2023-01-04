cf1e567309b239243b030b0bdef7ae4830d295f3
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:12:22 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/거듭제곱 구하기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 거듭제곱_구하기
+{
+    internal class Program
+    {
+        static void Square(double number)
+        {
+            double result = number * number;
+        }
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            Square(num);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
