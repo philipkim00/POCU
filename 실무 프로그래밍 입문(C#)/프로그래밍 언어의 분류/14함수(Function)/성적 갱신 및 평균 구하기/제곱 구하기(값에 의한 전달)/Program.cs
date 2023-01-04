d37d727e20e80426a243c59742885c9d0b36cda6
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:13:20 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/제곱 구하기(값에 의한 전달)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 제곱_구하기_값에_의한_전달_
+{
+    internal class Program
+    {
+        static void Square(double number)
+        {
+            number *=  number;
+        }
+        static void Main(string[] args)
+        {
+            double number = 5;
+            Console.WriteLine($"Before: {number}");
+            Square(number);
+            Console.WriteLine($"After: {number}");
+        }
+    }
+}
