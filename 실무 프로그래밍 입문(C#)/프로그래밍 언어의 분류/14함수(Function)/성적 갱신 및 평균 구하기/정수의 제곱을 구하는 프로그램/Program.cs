5f3357cc586dbdd12e29f6223c5c7672c169a29d
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:12:46 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/정수의 제곱을 구하는 프로그램/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace 정수의_제곱을_구하는_프로그램
+{
+    internal class Program
+    {
+        static double Square(double number)
+        {
+            number *= number;
+
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            double number = 5;
+            Console.WriteLine(Square(number));
+        }
+    }
+}
