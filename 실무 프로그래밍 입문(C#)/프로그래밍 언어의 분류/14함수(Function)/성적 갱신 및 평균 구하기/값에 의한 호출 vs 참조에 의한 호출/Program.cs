1f0ff5d88d3fa39b652e307d2e88d91dde74e4dc
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:14:04 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/값에 의한 호출 vs 참조에 의한 호출/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace 값에_의한_호출_vs_참조에_의한_호출
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+
+            IncrementByValye(num1, 2, 5);
+
+            Console.WriteLine($"num1 after IncrementByValue(): {num1}");
+
+            IncrementByRefererence(ref num1, 2, 5);
+
+            Console.WriteLine($"num1 after IncrementByRefererence(): {num1}");
+        }
+
+        static void IncrementByValye(int num, int increment, int incrementCount)
+        {
+            for (int i = 0; i < incrementCount; i++)
+            {
+                num += increment;
+            }
+        }
+        static void IncrementByRefererence(ref int num, int increment, int incrementCount)
+        {
+            for (int i = 0; i < incrementCount; i++)
+            {
+                num += increment;
+            }
+        }
+    }
+}
