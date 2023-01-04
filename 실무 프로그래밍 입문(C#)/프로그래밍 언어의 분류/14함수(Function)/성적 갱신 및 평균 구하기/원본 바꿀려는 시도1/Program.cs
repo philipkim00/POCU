fbb2c9c5bc390e6783b2df86b058e9101750cc18
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:13:33 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/원본 바꿀려는 시도1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace 원본_바꿀려는_시도1
+{
+    internal class Program
+    {
+        static double Square(double number)
+        {
+           return number *= number;
+        }
+        static void Main(string[] args)
+        {
+            double number = 5;
+            Console.WriteLine($"Before: {number}");
+            number = Square(number);
+            Console.WriteLine($"After: {number}");
+        }
+    }
+}
