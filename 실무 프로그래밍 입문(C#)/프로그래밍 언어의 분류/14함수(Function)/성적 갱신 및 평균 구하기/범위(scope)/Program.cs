e67762f20ed411b64f4535170a48b0864c93976f
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-04 22:13:00 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/범위(scope)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace 범위_scope_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] {1, -3, 4, -10, 4, 2, 9, 15 };
+            double average = GetAverage(numbers);
+
+            Console.WriteLine($"average: {average}");
+
+            {
+                // int average = 0; // 컴파일 에러
+                string message = "Message in the first child scope!";
+                Console.WriteLine(message);
+            }
+
+            {
+                //int average = 0; // 컴파일 에러
+                string message = "Message in the second chilld scope!";
+                Console.WriteLine(message);
+            }
+        }
+
+        static double GetAverage(int[] inputs) 
+        {
+            int sum = 0;
+
+            //  for (int i = 0; i < numbers.Length; i++)
+            //  {
+            //    sum += numbers[i];
+            //  }
+
+            for (int i = 0; i < inputs.Length; i++) 
+            {
+                sum += inputs[i];
+            }
+
+            return (double)sum / inputs.Length;
+        }
+    }
+}
