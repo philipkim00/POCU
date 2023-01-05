f1ae98ed412f17499f0c3569519a69a0bd59fd9a
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-05 20:53:41 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/성적 평균 구하기(함수로 분리)/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace 성적_평균_구하기_함수로_분리_
+{
+    internal class Program
+    {
+        static double CalculateAverage(double data, int count)
+        {
+            data /= count;
+            data = (int)(data * 10 + 0.5) / 10.0;
+
+            return data;
+        }
+
+        static void PrintAverageScore(double average, int count)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"{count} scores average: {average:f1}");
+            Console.WriteLine("---------------------------");
+        }
+        static void Main(string[] args)
+        {
+            double[] scores = { 100.0, 35.4, 75.4, 54.6, 99.5 };
+
+            double average = 0;
+            for (int i = 0; i < scores.Length; ++i)
+            {
+                average += scores[i];
+            }
+
+            average = CalculateAverage(average, scores.Length);
+            PrintAverageScore(average, scores.Length);
+
+            while (true)
+            {
+                for (int i = 0; i < scores.Length; ++i)
+                {
+                    Console.WriteLine($"Score[i]:  {scores[i]}");
+                }
+
+                Console.WriteLine($"While score do you want to update?(0~{scores.Length - 1})");
+
+                int index = int.Parse(Console.ReadLine());
+
+                if (index < 0 || index >= scores.Length)
+                {
+                    Console.WriteLine("Wrong index.");
+                }
+                else
+                {
+                    Console.Write("Enter the score: ");
+                    scores[index] = double.Parse(Console.ReadLine());
+
+                    average = 0;
+                    for (int i = 0; i < scores.Length; ++i)
+                    {
+                        average += scores[i];
+                    }
+
+                    average = CalculateAverage(average, scores.Length);
+                    PrintAverageScore(average, scores.Length);
+                }
+            }
+        }
+    }
+}
