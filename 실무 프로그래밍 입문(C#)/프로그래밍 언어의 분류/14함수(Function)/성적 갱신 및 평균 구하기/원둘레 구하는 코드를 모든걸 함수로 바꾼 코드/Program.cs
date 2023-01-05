60a5d25e020d37b02f3e4d63e4c672b95a1bde5d
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-05 20:53:58 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/원둘레 구하는 코드를 모든걸 함수로 바꾼 코드/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace 원둘레_구하는_코드를_모든걸_함수로_바꾼_코드
+{
+    class Program
+    {
+        static double GetRadius()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double CalculateCircumference(double r)
+        {
+            return 2 * 3.14 * r;
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.Write(message);
+        }
+
+        static void PrintRadius()
+        {
+            PrintMessage("Enter radius: ");
+            double radius = GetRadius();
+
+            double result = CalculateCircumference(radius);
+            PrintMessage($"Radius = {radius} Circumference = {result}\n");
+
+          }
+
+        static void Main(string[] args)
+        {
+            PrintRadius();
+        }
+    }
+}
+
