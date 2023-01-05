4b9ab7fb5cd960216558392044aeaf8bd672f72d
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-05 20:53:06 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/14함수(Function)/성적 갱신 및 평균 구하기/원둘레 구하기(함수로 안 분리)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 원둘레_구하기_함수로_안_분리_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double result = 2 * 3.14 * radius;
+            Console.WriteLine($"Radius = {radius} Circumference = {result}");
+        }
+    }
+}
