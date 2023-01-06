bc7474de4d68075a0a7fb010ccfcac36b651928e
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-06 21:18:12 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/15열거형(enum) 및 어서트(assert)/약간 수정한 동서남북 이동 프로그램/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace 약간_수정한_동서남북_이동_프로그램
+{
+    internal class Program
+    {
+        static void Move(int direction, ref int x, ref int y)
+        {
+            switch (direction)
+            {
+                case NORTH :
+                    y += 1;
+                    break;
+                case SOUTH:
+                    y -= 1;
+                    break;
+                case EAST:
+                    x += 1;
+                    break;
+                case WEST:
+                    x += 1;
+                    break;
+                default:
+                    Console.WriteLine($"You enter wrong direction!: {direction}");
+                    break;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int direction;
+            int x = 0;
+            int y = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Your location: ({x}, {y})");
+                Console.WriteLine("Where do you want go? (N:1, S:2, E:3, W:4)");
+
+                direction = int.Parse( Console.ReadLine() );
+                Move(direction, ref x, ref y);
+            }
+        }
+    }
+}
