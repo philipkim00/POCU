43f0feb58474086827c1b1a4a5004a25861a65e1
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-06 21:18:31 +0900

    Add files via upload

--- 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/15열거형(enum) 및 어서트(assert)/추억의 머드게임/Program.cs
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/15열거형(enum) 및 어서트(assert)/추억의 머드게임/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace 추억의_머드게임
 {
@@ -10,6 +7,39 @@ namespace 추억의_머드게임
     {
         static void Main(string[] args)
         {
+            const int NORTH = 1;
+            const int SOUTH = 2;
+            const int EAST = 3; 
+            const int WEST = 4;
+
+            int direction;
+            int x = 0;
+            int y = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"Your location: ({x}, {y})");
+                Console.WriteLine("Where do you want go? (N:1, S:2, E:3, W:4");
+                direction = int.Parse(Console.ReadLine());
+                switch (direction)
+                {
+                    case NORTH:
+                        y += 1;
+                        break;
+                    case SOUTH:
+                        y += 1;
+                        break;
+                    case EAST:
+                        x += 1;
+                        break;
+                    case WEST:
+                        x -= 1;
+                        break;
+                    default:
+                        Console.WriteLine($"You enter wrong direction!: {direction}");
+                        break;
+                }
+            }
         }
     }
 }
