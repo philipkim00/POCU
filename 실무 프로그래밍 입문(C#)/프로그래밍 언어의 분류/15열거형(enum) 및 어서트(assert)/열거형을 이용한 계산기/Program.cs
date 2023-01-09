e78ad3a4e778c2e4467b4dc8f89a48dfc393ccdf
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-09 19:55:37 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/15열거형(enum) 및 어서트(assert)/열거형을 이용한 계산기/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace 열거형을_이용한_계산기
+{
+    internal class Program
+    {
+        enum EOperator
+        {
+            Plus = '+',
+            Minus = '-',
+            Multiply = '*',
+            Divide = '/',
+            Mod = '%'
+
+        };
+        static void Main(string[] args)
+        {
+            Console.Write("num1: ");
+            string num1String = Console.ReadLine();
+            int num1 = int.Parse(num1String);
+
+            Console.Write("num2: ");
+            string num2String = Console.ReadLine();
+            int num2 = int.Parse(num2String);
+
+            Console.Write("operation (+, -, *, /, %): ");
+            string operationString = Console.ReadLine();
+            char operationChar = char.Parse(operationString);
+
+            EOperator operation = (EOperator)operationChar;
+
+            switch (operation)
+            {
+                case EOperator.Plus:
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+
+                case EOperator.Minus:
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+
+                case EOperator.Multiply:
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+
+                case EOperator.Divide:
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    break;
+
+                default:
+                    Console.WriteLine($"You entered a wrong operator: {operationChar}");
+                    break;
+
+            }
+
+        }
+    }
+}
