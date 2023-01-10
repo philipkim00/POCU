1e80139a67c9cb6963ed9e041e0467d65ff2e7b2
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-10 20:53:24 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/16재귀함수,그리고 주사위 굴리고(랜덤 수 생성)/피보나치 수/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace 피보나치_수
+{
+    internal class Program
+    {
+        public static int FibonacciRecursive(uint number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return FibonacciRecursive(number - 2) + FibonacciRecursive(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibonacciRecursive(10));
+        }
+    }
+}
