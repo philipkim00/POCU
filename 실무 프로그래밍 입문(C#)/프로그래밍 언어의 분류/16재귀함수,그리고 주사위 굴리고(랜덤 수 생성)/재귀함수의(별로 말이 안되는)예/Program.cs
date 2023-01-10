57060d62942b27d4d8efe2352357d3c42c028b2b
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-10 20:53:06 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/16재귀함수,그리고 주사위 굴리고(랜덤 수 생성)/재귀함수의(별로 말이 안되는)예/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 재귀함수의_별로_말이_안되는_예
+{
+    internal class Program
+    {
+        static uint SumRecursive(uint num)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return num + SumRecursive(num - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumRecursive(3));
+        }
+    }
+}
