5f3960bca5da0b994c6a08d121d67560fa89f878
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-11 21:26:33 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/16재귀함수,그리고 주사위 굴리고(랜덤 수 생성)/언제나 정해진 게임/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace 언제나_정해진_게임
+{
+    enum EShape
+    {
+        Rock,
+        Scissor,
+        Paper
+    };
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EShape computer = EShape.Paper;
+
+            while(true)
+            {
+                Console.WriteLine("Rock(0)~ paper(1)~ scissor(2)~: ");
+                EShape user = (EShape)int.Parse(Console.ReadLine());
+
+                if (computer == user)
+                {
+                    Console.WriteLine("Draw");
+                }
+                else if ((computer == EShape.Rock && user == EShape.Scissor)
+                    || (computer == EShape.Paper && user == EShape.Rock)
+                    || (computer == EShape.Scissor && user == EShape.Paper))
+
+                {
+                    Console.WriteLine("Lose");
+                }
+
+                else if ((computer == EShape.Rock && user == EShape.Paper)
+                    || (computer == EShape.Paper && user == EShape.Scissor)
+                    || (computer == EShape.Scissor && user == EShape.Rock))
+                {
+                    Console.WriteLine("Win");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input!");
+                }
+            }
+        }
+    }
+}
