d52198deebdf21248fbce76f2d0ca000953184f0
philipkim00 <107772629+philipkim00@users.noreply.github.com>
2023-01-12 19:58:55 +0900

    Add files via upload

---
+++ 실무 프로그래밍 입문(C#)/프로그래밍 언어의 분류/17배열의 배열, 문자열 분할/2D배열을 이용한 학생 명부/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2D배열을_이용한_학생_명부
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CLASS_COUNT = 3;
+            const int STUDENT_COUNT = 5;
+
+            string[,] classrooms = new string[CLASS_COUNT, STUDENT_COUNT]
+                {
+                    { "Severus","Draco", "Bellatrix", "", "" },
+                    { "Cedric", "Hannah", "", "", "" },
+                    { "Hermione", "Harry", "Leville", "Parvati", "Ron" }
+                };
+            
+            for (int i = 0; i < CLASS_COUNT; ++i)
+            {
+                Console.WriteLine($"Class{i + 1} Info*****************");
+                for (int j = 0; j < STUDENT_COUNT; ++j )
+                {
+                    Console.WriteLine($"Student{j + 1} name: {classrooms[i, j]}");
+                }
+                Console.WriteLine("*******************************");
+            }
+
+        }
+    }
+}
